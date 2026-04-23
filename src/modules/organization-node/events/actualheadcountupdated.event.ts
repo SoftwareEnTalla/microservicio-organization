d0642169816a168e3bb87a282e76827d59c132b7
SoftwarEnTalla <softwarentalla@gmail.com>
2026-04-22 21:35:49 -0700

    Commit automático previo a pull --rebase (script)

--- src/modules/organization-node/events/actualheadcountupdated.event.ts
+++ src/modules/organization-node/events/actualheadcountupdated.event.ts
@@ -34,7 +34,7 @@ import { OrganizationNode } from '../entities/organization-node.entity';
 import { BaseEvent, PayloadEvent } from './base.event'; 
 import { v4 as uuidv4 } from "uuid";
 
-export class OrganizationNodeUpdatedEvent extends BaseEvent {
+export class ActualHeadcountUpdatedEvent extends BaseEvent {
   constructor(
     public readonly aggregateId: string,
     public readonly payload: PayloadEvent<UpdateOrganizationNodeDto|OrganizationNode>
@@ -49,8 +49,8 @@ export class OrganizationNodeUpdatedEvent extends BaseEvent {
           instance: UpdateOrganizationNodeDto|OrganizationNode,
           userId: string,
           correlationId?: string
-        ): OrganizationNodeUpdatedEvent {
-          return new OrganizationNodeUpdatedEvent(instanceId, {
+        ): ActualHeadcountUpdatedEvent {
+          return new ActualHeadcountUpdatedEvent(instanceId, {
             instance: instance,
             metadata: {
               initiatedBy: userId,
--- src/modules/organization-node/events/targetheadcountupdated.event.ts
+++ src/modules/organization-node/events/targetheadcountupdated.event.ts
@@ -34,7 +34,7 @@ import { OrganizationNode } from '../entities/organization-node.entity';
 import { BaseEvent, PayloadEvent } from './base.event'; 
 import { v4 as uuidv4 } from "uuid";
 
-export class OrganizationNodeUpdatedEvent extends BaseEvent {
+export class TargetHeadcountUpdatedEvent extends BaseEvent {
   constructor(
     public readonly aggregateId: string,
     public readonly payload: PayloadEvent<UpdateOrganizationNodeDto|OrganizationNode>
@@ -49,8 +49,8 @@ export class OrganizationNodeUpdatedEvent extends BaseEvent {
           instance: UpdateOrganizationNodeDto|OrganizationNode,
           userId: string,
           correlationId?: string
-        ): OrganizationNodeUpdatedEvent {
-          return new OrganizationNodeUpdatedEvent(instanceId, {
+        ): TargetHeadcountUpdatedEvent {
+          return new TargetHeadcountUpdatedEvent(instanceId, {
             instance: instance,
             metadata: {
               initiatedBy: userId,
